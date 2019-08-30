f19f1d17340795f2a18808f1dfff0bba1956a51c
DAW <madronadan@gmail.com>
2019-08-30 15:05:06 -0700

    Removed line in nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,6 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      { rel: 'stylesheet'; href: 'https://fonts.googleapis.com/css?family=Electrolize|Farro&display=swap'}
     ]
   },
   /*
